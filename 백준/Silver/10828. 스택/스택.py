c7c6c8bc52bf7648a2111fe58bc9e01f744c73a3
JIYUN02 <165583918+JIYUN02@users.noreply.github.com>
2024-09-23 00:10:59 +0900

    [Silver IV] Title: 스택, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,25 @@
+import sys
+a=int(sys.stdin.readline())
+stack=[]
+
+for i in range(a):
+    b=sys.stdin.readline().split()
+    if b[0]=='push':
+        stack.append(b[1])
+    elif b[0]=='pop':
+        if len(stack)==0:
+            print(-1)
+        else:
+            print(stack.pop())
+    elif b[0]=='size':
+        print(len(stack))
+    elif b[0]=='empty':
+        if len(stack)==0:
+            print(1)
+        else:
+            print(0)
+    elif b[0]=='top':
+        if len(stack)==0:
+            print(-1)
+        else:
+            print(stack[-1])
