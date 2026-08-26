8cef6ff524c4652f320e6fe5fc9e2dcdc4140e37
JIYUN02 <165583918+JIYUN02@users.noreply.github.com>
2026-08-27 02:40:05 +0900

    [level 0] Title: 배열 만들기 3, Time: 1.54 ms, Memory: 13.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.py
@@ -0,0 +1,10 @@
+def solution(arr, intervals):
+    result = []
+
+    for i in range(intervals[0][0], intervals[0][1] + 1):
+        result.append(arr[i])
+
+    for j in range(intervals[1][0], intervals[1][1] + 1):
+        result.append(arr[j])
+
+    return result
